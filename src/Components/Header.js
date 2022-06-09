54c58d7d01f60f4151becc48d6822c217ee5d956
Jigyasa Upadhyay <64299683+jigyasa-ctrl@users.noreply.github.com>
2022-06-09 21:55:09 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     return (
         <div className="header">
             <h1>Type the Alphabet</h1>
-            <p>Typing game to see how fast you type. Timer startrs when you do :)</p>
+            <p>Typing game to see how fast you type. Timer starts when you do :)</p>
         </div>
     )
 }
